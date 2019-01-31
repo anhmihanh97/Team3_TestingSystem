6b745eb8f610c94ea199087d23be2a3532e085b4
anhmihanh97 <anhmihanh97@gmail.com>
2019-01-31 16:56:19 +0700

    update 31-01-2019

---
+++ TestingSys/src/main/java/com/cmcglobal/TestingSysApplication.java
@@ -0,0 +1,14 @@
+package com.cmcglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSysApplication.class, args);
+	}
+
+}
+
